02a99a474753394f9d92d7640095b330c738932b
Allen_359857508 <allen359857508@gmail.com>
2024-01-26 22:48:50 +1100

    unit test practice

--- src/components/MouseTracker.test.js
+++ src/components/MouseTracker.test.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import Mousetracker from './Mousetracker';
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import MouseTracker from "./MouseTracker";
 
 describe('MouseTracker', () => {
-  test('tracks mouse movement', () => {
-    const renderMock = jest.fn().mockImplementation(({ x, y }) => (
-      <div>
-        Mouse position is x: {x}, y: {y}.
-      </div>
-    ));
-    render(<Mousetracker render={renderMock} />);
-    const mouseEvent = new MouseEvent('mousemove', {
-      clientX: 100,
-      clientY: 200,
-    });
-    fireEvent(document, mouseEvent);
-    expect(renderMock), toHaveBeenCalledWith({ x: 100, y: 200 });
-    expect(
-      screen.getByText('Mouse position is x: 100, y: 200.')
-    ).toBeInTheDocument();
+  it('track mouse move', () => {
+    const renderMock = jest.fn(({ x, y }) => <div>Mouse position is {x}, {y}</div>);
+
+    const { container } = render(<MouseTracker render={renderMock} />);
+    fireEvent.mouseMove(container.firstChild, { clientX: 100, clientY: 200 });
+
+    expect(renderMock).toHaveBeenCalledWith({ x: 100, y: 200 });
+    expect(container.textContent).toBe('Mouse position is 100, 200');
+  });
+
+  test('check snapshot', () => {
+    const renderMock = jest.fn(() => <div>Mouse position is 100, 200</div>);
+    const tree = renderer.create(<MouseTracker render={renderMock} />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
